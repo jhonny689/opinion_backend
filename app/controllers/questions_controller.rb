48fe10c32fa79860f89bb8b11a28dc3310ce8049
jhonny689 <jhonny.chamoun@gmail.com>
2020-09-27 19:45:07 -0400

    adding index functionality for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,9 @@
 class QuestionsController < ApplicationController
+    def index
+        answers = Question.all
+        if params[:survey]
+            answers = Question.all.filter{|ans| ans.survey_id.to_s == params[:survey]}
+        end
+        render json: answers
+    end
 end
--- app/serializers/survey_serializer.rb
+++ app/serializers/survey_serializer.rb
@@ -1,4 +1,4 @@
 class SurveySerializer
   include FastJsonapi::ObjectSerializer
-  attributes :title, :status, :due_date, :description, :surveyor, :survey_category_id, :questions
+  attributes :id, :title, :status, :due_date, :description, :surveyor, :survey_category_id, :questions
 end
