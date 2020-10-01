b190b942e6a7c3ee7e57bce0092abd5ec7abccf9
jhonny689 <jhonny.chamoun@gmail.com>
2020-10-01 11:01:46 -0400

    thursday morning

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,9 @@
 class QuestionsController < ApplicationController
     def index
-        answers = Question.all
+        questions = Question.all
         if params[:survey]
-            answers = Question.all.filter{|ans| ans.survey_id.to_s == params[:survey]}
+            questions = Question.all.filter{|ans| ans.survey_id.to_s == params[:survey]}
         end
-        render json: answers
+        render json: questions
     end
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,16 @@
 class Response < ApplicationRecord
   belongs_to :user
   belongs_to :question
+  
+  def survey
+    self.question.survey
+  end
+
+  def question_text
+    self.question.question_text
+  end
+
+  def question_type
+    self.question.question_type_id
+  end
 end
